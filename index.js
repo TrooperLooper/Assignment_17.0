be8ad81e6e6332b4ae0429baed5a69d623dc5208
Lars <lars@recap.dk>
2025-03-22 22:18:13 +0100

    final update
    cleaned up the code. done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 let price = prompt ("Enter pricetag (fx. $49.99)", "$");
-let priceNumber = parseFloat(price);
-
 price = price.replace("$", "");
+
+let priceNumber = parseFloat(price);
 
 let discount = priceNumber*0.10;
 let reducedPrice = priceNumber - discount;
 
-console.log(reducedPrice);
 console.log("your discount price is $" + reducedPrice);
